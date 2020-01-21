8eacd50337adf6c405ebbc757fb1298ed538d5f5
Lu Yi <leo0650@gmail.com>
2020-01-21 23:22:23 +0800

    Update

--- fix_macos_ql.sh
+++ fix_macos_ql.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# https://github.com/anthonygelibert/QLColorCode/issues/51
 
 find /usr/local/Caskroom -name "*.qlgenerator" -print0 | xargs -0 xattr -d com.apple.quarantine 2>/dev/null
 qlmanage -r 
