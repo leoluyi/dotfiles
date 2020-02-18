281ee16c2262b72e3ba433af3407f875318e5270
Lu Yi <leo0650@gmail.com>
2020-02-18 22:33:44 +0800

    Update

--- fix_macos_ql.sh
+++ fix_macos_ql.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # https://github.com/anthonygelibert/QLColorCode/issues/51
+# https://github.com/whomwah/qlstephen/issues/81#issuecomment-582207278
 
-find /usr/local/Caskroom -name "*.qlgenerator" -print0 | xargs -0 xattr -d com.apple.quarantine 2>/dev/null
-qlmanage -r 
+# find /usr/local/Caskroom -name "*.qlgenerator" -print0 | xargs -0 xattr -d com.apple.quarantine 2>/dev/null
+find /usr/local/Caskroom -name "*.qlgenerator" -print0 | xargs -I_ -0 xattr -cr _ 2>/dev/null
+# xattr -cr ~/Library/QuickLook/QLStephen.qlgenerator
+
+qlmanage -r
