46d324685dfc6b0e953d0cdde32bcb4228b36e0e
Lu Yi <leo0650@gmail.com>
2021-01-02 17:49:43 +0800

    Update

--- r_and_python_install/mac-install-R.sh
+++ r_and_python_install/mac-install-R.sh
@@ -12,42 +12,29 @@ brew update
 brew doctor
 brew cleanup
 
-pip install pelican==3.6.3 
-pip install markdown rpy2
+# Install R
+if ! command -v nvim &>/dev/null || [ "$1" = "-f" ]; then
+  brew install openblas
+  brew install r
+  defaults write org.R-project.R force.LANG en_US.UTF-8
+  # echo 'Sys.setlocale(category="LC_ALL", locale = "en_US.UTF-8")' >> ~/.bash_profile
 
-# R
-brew install openblas
-brew install r --with-openblas
-defaults write org.R-project.R force.LANG en_US.UTF-8
-# echo 'Sys.setlocale(category="LC_ALL", locale = "en_US.UTF-8")' >> ~/.bash_profile
-
-# data.table
-R --vanilla << EOF
+  # data.table
+  R --vanilla << EOF
 install.packages('data.table', repos='https://cloud.r-project.org')
 q()
 EOF
 
-# knitr
-R --vanilla << EOF
-install.packages('knitr', repos='https://cloud.r-project.org')
-q()
-EOF
+  # rJava
+  brew install --cask java
+  # ln -f -s "$(/usr/libexec/java_home)/jre/lib/server/libjvm.dylib" /usr/local/lib
+  sudo R CMD javareconf
+  Rscript -e 'install.packages("rJava", repos="https://cloud.r-project.org")';
 
-# rmarkdown
-R --vanilla << EOF
-install.packages('rmarkdown', repos='http://cran.us.r-project.org')
-q()
-EOF
+  # devtools
+  brew install openssl openssl@1.1 libssh2
+  Rscript -e 'install.packages("devtools", repos="https://cloud.r-project.org")';
 
-# rJava
-brew cask install java
-# ln -f -s "$(/usr/libexec/java_home)/jre/lib/server/libjvm.dylib" /usr/local/lib
-sudo R CMD javareconf
-Rscript -e 'install.packages("rJava", repos="https://cloud.r-project.org")';
-
-# devtools
-brew install openssl openssl@1.1 libssh2
-Rscript -e 'install.packages("devtools", repos="https://cloud.r-project.org")';
-
-# tidyverse tools
-Rscript -e 'install.packages("tidyverse", repos="https://cloud.r-project.org")';
+  # tidyverse tools
+  Rscript -e 'install.packages("tidyverse", repos="https://cloud.r-project.org")';
+fi
