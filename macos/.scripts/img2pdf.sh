814531887dd28b23f4c436577f401d1e78d045ff
Lu Yi <leo0650@gmail.com>
2021-06-14 00:12:50 +0800

    Update script

--- macos/.scripts/img2pdf.sh
+++ macos/.scripts/img2pdf.sh
@@ -10,13 +10,14 @@ echo -e "Find files:\n${img_files}"
 
 # Move original files to tempdir.
 tempdir=$(mktemp -d)
-echo "$img_files" | xargs -d"\n" -P0 -I_ cp _ -t "$tempdir"
+echo "Tempdir: ${tempdir}"
+echo "$img_files" | xargs -d"\n" -P0 -I_ cp "_" -t "$tempdir"
 img_files=$(find "$tempdir" -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.jpg' \) | sort -n)
 
 # https://askubuntu.com/a/987341
 echo
 echo "1. Detecting min width among images..."
-min_width=$(echo "$img_files" | xargs -L1 identify -format '%w\n' | sort -n | head -1)
+min_width=$(echo "$img_files" | xargs -L1 -I_ identify -format '%w\n' "_" | sort -n | head -1)
 
 if [ "$min_width" -gt 1280 ]; then
   echo "(Original min_width ${min_width}px is too big. Use 1280px instead.)"
@@ -27,8 +28,11 @@ fi
 # https://stackoverflow.com/a/28937338/3744499
 echo
 echo "2. Remove alpha channel in images and resize..."
-echo "$img_files" | xargs -d"\n" -P0 -I_ convert _ -resize "${min_width}x" -background black -alpha remove -alpha off _
+echo "$img_files" | xargs -d"\n" -P0 -I_ convert "_" -resize "${min_width}x" -background black -alpha remove -alpha off "_"
 
 echo
 echo "3. Combining images to 'output.pdf'"
+old_ifs="$IFS"
+IFS=$'\n'
 convert -quality 40 $(echo "$img_files") ouput.pdf
+IFS="$old_ifs"
