33ee3ad7b5d0f4a8031701b366d5dc41f922ad69
Lu Yi <leo0650@gmail.com>
2024-12-07 00:33:02 +0800

    WIP: Sat Dec  7 00:33:02 CST 2024

--- nvim/.config/nvim/lua/plugins/editor/hop.lua
+++ nvim/.config/nvim/lua/plugins/editor/hop.lua
@@ -1,27 +1,27 @@
 return {
-  -- easily jump to any location and enhanced f/t motions for Leap.
-  -- < https://github.com/phaazon/hop.nvim >
-  {
-    'phaazon/hop.nvim',
-    branch = 'v2', -- optional but strongly recommended
-    keys = function ()
-      -- local hop = require('hop')
-      -- local directions = require('hop.hint').HintDirection
-
-      return {
-        { '<localleader><localleader>', '<cmd>HopWord<CR>', desc = "Hop Word" },
-        -- { 'f', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true }) end, },
-        -- { 'F', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true }) end, },
-        -- { 't', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end, },
-        -- { 'T', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end, },
-      }
-    end,
-    opts = {
-      keys = 'etovxqpdygfblzhckisuran',
-    },
-    config = function(_, opts)
-      require'hop'.setup(opts)
-    end
-  },
+  -- {
+  --   -- easily jump to any location and enhanced f/t motions for Leap.
+  --   -- < https://github.com/phaazon/hop.nvim >
+  --   'phaazon/hop.nvim',
+  --   branch = 'v2', -- optional but strongly recommended
+  --   keys = function ()
+  --     -- local hop = require('hop')
+  --     -- local directions = require('hop.hint').HintDirection
+  --
+  --     return {
+  --       { '<localleader><localleader>', '<cmd>HopWord<CR>', desc = "Hop Word" },
+  --       -- { 'f', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true }) end, },
+  --       -- { 'F', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true }) end, },
+  --       -- { 't', function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end, },
+  --       -- { 'T', function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end, },
+  --     }
+  --   end,
+  --   opts = {
+  --     keys = 'etovxqpdygfblzhckisuran',
+  --   },
+  --   config = function(_, opts)
+  --     require'hop'.setup(opts)
+  --   end
+  -- },
 
 }
