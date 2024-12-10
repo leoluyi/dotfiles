4bc1894bfbf48bca9837a16c882273cef1590af4
Lu Yi <leo0650@gmail.com>
2024-12-11 00:05:10 +0800

    WIP: Wed Dec 11 00:05:10 CST 2024

--- common_dotfiles/.config/wezterm/helpers.lua
+++ common_dotfiles/.config/wezterm/helpers.lua
@@ -17,3 +17,47 @@ wezterm.on('toggle-colorscheme', function(window, pane)
   end
   window:set_config_overrides(overrides)
 end)
+
+M.theme_switcher = function(window, pane)
+  -- get builting color schemes
+  local schemes = wezterm.get_builtin_color_schemes()
+  local choices = {}
+  local config_path = "~/.config/wezterm/wezterm.lua"
+
+  -- populate theme names in choices list
+  for key, _ in pairs(schemes) do
+    table.insert(choices, { label = tostring(key) })
+  end
+
+  -- sort choices list
+  table.sort(choices, function(c1, c2)
+    return c1.label < c2.label
+  end)
+
+  window:perform_action(
+    act.InputSelector({
+      title = "🎨 Pick a Theme!",
+      choices = choices,
+      fuzzy = true,
+
+      -- execute 'sed' shell command to replace the line
+      -- responsible of colorscheme in my config
+      action = wezterm.action_callback(function(inner_window, inner_pane, _, label)
+        inner_window:perform_action(
+          act.SpawnCommandInNewTab({
+            args = {
+              "sed",
+              "-i",
+              "'s/color_scheme.*$/color_scheme = \"" .. label .. "\"/'",
+              config_path,
+            },
+          }),
+          inner_pane
+        )
+      end),
+    }),
+    pane
+  )
+end
+
+return M
