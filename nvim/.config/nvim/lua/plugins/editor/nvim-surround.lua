e44cb61c3410f919da2f71b8aa2aab204abf1739
Lu Yi <leo0650@gmail.com>
2024-12-11 22:37:55 +0800

    WIP: Wed Dec 11 22:37:55 CST 2024

--- nvim/.config/nvim/lua/plugins/editor/nvim-surround.lua
+++ nvim/.config/nvim/lua/plugins/editor/nvim-surround.lua
@@ -1,5 +1,6 @@
 return {
   {
+    -- < https://github.com/kylechui/nvim-surround >
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
