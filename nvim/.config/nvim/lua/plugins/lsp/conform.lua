b87effa9d572179f35b400d23302d02485adf3d7
Lu Yi <leo0650@gmail.com>
2024-12-11 17:11:22 +0800

    WIP: Wed Dec 11 17:11:22 CST 2024

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -1,5 +1,6 @@
 return {
   {
+    -- < https://github.com/stevearc/conform.nvim >
     -- Lightweight yet powerful formatter plugin for Neovim
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
@@ -18,6 +19,12 @@ return {
       },
     },
     opts = {
+      format_on_save = {
+        -- I recommend these options. See :help conform.format for details.
+        lsp_format = "fallback",
+        timeout_ms = 500,
+      },
+
       formatters_by_ft = {
         lua = { "stylua" },
         svelte = { { "prettierd", "prettier" } },
