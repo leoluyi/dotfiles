a80f1a903b7401c2f287828ee948dbb95ba0845e
Lu Yi <leo0650@gmail.com>
2025-01-01 00:53:02 +0800

    WIP: Wed Jan  1 00:53:02 CST 2025

--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -5,74 +5,79 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     dependencies = {
-      { "nvim-tree/nvim-web-devicons", lazy = true, },
+      { "nvim-tree/nvim-web-devicons", lazy = true },
     },
     opts = function()
       local function venv()
-        if vim.g.virtualenv_name then return vim.g.virtualenv_name else return '' end
+        if vim.g.virtualenv_name then
+          return vim.g.virtualenv_name
+        else
+          return ""
+        end
       end
       local icons = require("helpers.icons")
       local mode_map = {
-        ['n']    = 'N',
-        ['no']   = 'O-PENDING',
-        ['nov']  = 'O-PENDING',
-        ['noV']  = 'O-PENDING',
-        ['no'] = 'O-PENDING',
-        ['niI']  = 'N',
-        ['niR']  = 'N',
-        ['niV']  = 'N',
-        ['nt']   = 'N',
-        ['v']    = 'V',
-        ['vs']   = 'V',
-        ['V']    = 'VL',
-        ['Vs']   = 'VL',
-        ['']   = 'VB',
-        ['s']  = 'VB',
-        ['s']    = 'S',
-        ['S']    = 'SL',
-        ['']   = 'SB',
-        ['i']    = 'I',
-        ['ic']   = 'I',
-        ['ix']   = 'I',
-        ['R']    = 'R',
-        ['Rc']   = 'R',
-        ['Rx']   = 'R',
-        ['Rv']   = 'VR',
-        ['Rvc']  = 'VR',
-        ['Rvx']  = 'VR',
-        ['c']    = 'C',
-        ['cv']   = 'EX',
-        ['ce']   = 'EX',
-        ['r']    = 'R',
-        ['rm']   = 'MORE',
-        ['r?']   = 'CONFIRM',
-        ['!']    = 'SHELL',
-        ['t']    = 'TERM',
+        ["n"] = "N",
+        ["no"] = "O-PENDING",
+        ["nov"] = "O-PENDING",
+        ["noV"] = "O-PENDING",
+        ["no"] = "O-PENDING",
+        ["niI"] = "N",
+        ["niR"] = "N",
+        ["niV"] = "N",
+        ["nt"] = "N",
+        ["v"] = "V",
+        ["vs"] = "V",
+        ["V"] = "VL",
+        ["Vs"] = "VL",
+        [""] = "VB",
+        ["s"] = "VB",
+        ["s"] = "S",
+        ["S"] = "SL",
+        [""] = "SB",
+        ["i"] = "I",
+        ["ic"] = "I",
+        ["ix"] = "I",
+        ["R"] = "R",
+        ["Rc"] = "R",
+        ["Rx"] = "R",
+        ["Rv"] = "VR",
+        ["Rvc"] = "VR",
+        ["Rvx"] = "VR",
+        ["c"] = "C",
+        ["cv"] = "EX",
+        ["ce"] = "EX",
+        ["r"] = "R",
+        ["rm"] = "MORE",
+        ["r?"] = "CONFIRM",
+        ["!"] = "SHELL",
+        ["t"] = "TERM",
       }
 
       return {
         options = {
           icons_enabled = true,
-          theme = 'auto',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
+          theme = "auto",
+          component_separators = { left = "", right = "" },
+          -- section_separators = { left = '', right = ''},
+          section_separators = { left = "", right = "" },
           disabled_filetypes = {
-            'dashboard',
-            'alpha',
+            "dashboard",
+            "alpha",
           },
           always_divide_middle = true,
           globalstatus = true,
         },
         sections = {
           lualine_a = {
-            function ()
+            function()
               local paste_mode = vim.go.paste and " (PASTE)" or ""
               return (mode_map[vim.api.nvim_get_mode().mode] .. paste_mode) or "_"
             end,
           },
           lualine_b = {
-            { 'branch', icon = '' },
-            { 'diagnostics' },
+            { "branch", icon = "" },
+            { "diagnostics" },
             { venv },
           },
           lualine_c = {
@@ -90,11 +95,11 @@ return {
             },
           },
           lualine_y = {
-            'encoding',
-            'fileformat',
-            'filetype',
-            { 'progress', separator = " ", padding = { left = 1, right = 0 } },
-            { 'location', padding = { left = 0, right = 1 } },
+            "encoding",
+            "fileformat",
+            "filetype",
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
           },
           lualine_z = {
             {
@@ -107,17 +112,17 @@ return {
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
           lualine_y = {},
-          lualine_z = {}
+          lualine_z = {},
         },
         tabline = {},
         extensions = { "lazy" },
       }
     end,
 
-    config = function (_, opts)
+    config = function(_, opts)
       -- Debug: Print the number of elements in opts.sections.lualine_x.
       -- print(#opts.sections.lualine_x)
 
