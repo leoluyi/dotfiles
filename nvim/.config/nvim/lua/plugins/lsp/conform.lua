3d07da3263ae208e0cab4e315f2fa906a8ae312f
Lu Yi <leo0650@gmail.com>
2025-02-25 22:44:28 +0800

    WIP: Tue Feb 25 22:44:28 CST 2025

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -15,7 +15,7 @@ return {
           })
         end,
         mode = { "n", "x" },
-        desc = "Format file or range",
+        desc = "(conform) Format file or range",
       },
     },
     opts = {
