438beb050387609463f0b9cf14cbf947ebf04ff2
Lu Yi <leo0650@gmail.com>
2026-02-10 18:24:28 +0800

    WIP: Tue Feb 10 18:24:28 CST 2026

---
+++ macos/setup-OpenInTerminal-Lite-CotEditor.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# < https://github.com/Ji4n1ng/OpenInTerminal/blob/master/Resources/README-Lite.md >
+
+defaults write wang.jianing.app.OpenInEditor-Lite LiteDefaultEditor CotEditor
+
+# Reset.
+# defaults remove wang.jianing.app.OpenInTerminal-Lite LiteDefaultTerminal
