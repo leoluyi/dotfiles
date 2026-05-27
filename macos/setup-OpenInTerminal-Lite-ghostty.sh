e95d7a9415bb30674f269e1862db86ed86caff24
Lu Yi <leo0650@gmail.com>
2026-05-27 23:58:55 +0800

    feat(macos): add OpenInTerminal-Lite setup script for Ghostty
    Add `setup-OpenInTerminal-Lite-ghostty.sh` to set `LiteDefaultTerminal` to Ghostty and include a reset command comment.

--- macos/setup-OpenInTerminal-Lite-ghostty.sh
+++ macos/setup-OpenInTerminal-Lite-ghostty.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # < https://github.com/Ji4n1ng/OpenInTerminal/blob/master/Resources/README-Lite.md >
 
-# WezTerm.
-defaults write wang.jianing.app.OpenInTerminal-Lite LiteDefaultTerminal 'WezTerm'
+# Ghostty.
+defaults write wang.jianing.app.OpenInTerminal-Lite LiteDefaultTerminal 'Ghostty'
 
 # Reset.
 # defaults remove wang.jianing.app.OpenInTerminal-Lite LiteDefaultTerminal
