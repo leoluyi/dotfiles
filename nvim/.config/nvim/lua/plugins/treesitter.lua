f7dbf902d0001d0e0efa575ce6490ed8bb4c8c05
Lu Yi <leo0650@gmail.com>
2026-08-07 23:58:02 +0800

    feat(treesitter): support Neovim 0.12 native Tree-sitter API
    Add version-aware configuration for Neovim 0.12, including:
    - Use the `main` branches of Tree-sitter and textobjects.
    - Initialize native parser installation, highlighting, indentation, and folding.
    - Configure textobject selection and movement via the new setup API.
    - Preserve legacy configuration and lazy-loading behavior for older Neovim versions.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,105 +1,139 @@
 -- < https://github.com/nvim-treesitter/nvim-treesitter >
 -- < https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/treesitter.lua >
 
+local is_nvim_012 = vim.fn.has("nvim-0.12") == 1
 local load_textobjects = false
+local parsers = {
+  "bash",
+  "cpp",
+  "css",
+  "dockerfile",
+  "go",
+  "gomod",
+  "html",
+  "java",
+  "javascript",
+  "json",
+  "json5",
+  "latex",
+  "lua",
+  "make",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "query",
+  "regex",
+  "rst",
+  "rust",
+  "scss",
+  "toml",
+  "vim",
+  "vimdoc",
+  "yaml",
+}
+
+local legacy_opts = {
+  auto_install = true,
+  ensure_installed = parsers,
+
+  ignore_install = {
+    -- Don't install comment!! It's too laggy < https://github.com/nvim-treesitter/nvim-treesitter/issues/5057#issuecomment-1617844020 >
+    "comment"
+  },
+
+  incremental_selection = {
+    enable = true,
+    disable = require("util.treesitter").disable_file_handle,
+    keymaps = {
+      init_selection = "<C-space>",
+      node_incremental = "<C-space>",
+      scope_incremental = false,
+      node_decremental = "<bs>",
+    },
+  },
+
+  indent = {
+    enable = true,
+    disable = require("util.treesitter").disable_file_handle,
+  },
+
+  highlight = {
+    enable = true,
+    disable = require("util.treesitter").disable_file_handle,
+    additional_vim_regex_highlighting = false,
+  },
+}
 
 return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    -- enabled = false,
+    branch = is_nvim_012 and "main" or "master",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
+    lazy = is_nvim_012 and false or nil,
+    event = is_nvim_012 and nil or { "BufReadPost", "BufNewFile" },
     dependencies = {
       {
-        {
-          "nvim-treesitter/nvim-treesitter-textobjects",
-          init = function()
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = is_nvim_012 and "main" or "master",
+        config = function()
+          if is_nvim_012 then
+            local ok, textobjects = pcall(require, "nvim-treesitter-textobjects")
+            if ok and textobjects.setup then
+              textobjects.setup({
+                select = { lookahead = true },
+                move = { set_jumps = true },
+              })
+            end
+          end
+        end,
+        init = function()
+          if not is_nvim_012 then
             -- disable rtp plugin, as we only need its queries for mini.ai
             -- In case other textobject modules are enabled, we will load them
             -- once nvim-treesitter is loaded
             require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
             load_textobjects = true
-          end,
-        },
+          end
+        end,
       },
     },
 
-    keys = {
+    keys = is_nvim_012 and {} or {
       { "<c-space>", desc = "(TS) Increment selection" },
       { "<bs>", desc = "(TS) Decrement selection", mode = "x" },
     },
 
-    -- @type TSConfig
-    opts = {
-      auto_install = true,
-      ensure_installed = {
-        "bash",
-        "cpp",
-        "css",
-        "dockerfile",
-        "go",
-        "gomod",
-        "html",
-        "java",
-        "javascript",
-        "json",
-        "json5",
-        "jsonc",
-        "latex",
-        "lua",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "rst",
-        "rust",
-        "scss",
-        "toml",
-        "vim",
-        "vimdoc",
-        "yaml",
-      },
-
-      ignore_install = {
-        -- Don't install comment!! It's too laggy < https://github.com/nvim-treesitter/nvim-treesitter/issues/5057#issuecomment-1617844020 >
-        "comment"
-      },
-
-      incremental_selection = {
-        enable = true,
-        disable = require("util.treesitter").disable_file_handle,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-
-      indent = {
-        enable = true,
-        disable = require("util.treesitter").disable_file_handle,
-      },
-
-      highlight = {
-        -- false will disable the whole extension
-        enable = true,
-
-        -- disable treesitter for large files
-        disable = require("util.treesitter").disable_file_handle,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
-    },
+    opts = is_nvim_012 and { ensure_installed = parsers } or legacy_opts,
 
     config = function(_, opts)
+
+      if is_nvim_012 then
+        local treesitter = require("nvim-treesitter")
+        treesitter.setup({
+          install_dir = vim.fn.stdpath("data") .. "/site",
+        })
+        treesitter.install(opts.ensure_installed)
+
+        vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+        local group = vim.api.nvim_create_augroup("UserTreesitter", {})
+        vim.api.nvim_create_autocmd("FileType", {
+          group = group,
+          pattern = "*",
+          callback = function(args)
+            local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+            if not lang or not pcall(vim.treesitter.start, args.buf, lang) then
+              return
+            end
+
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            vim.wo[0][0].foldmethod = "expr"
+            vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          end,
+        })
+        return
+      end
 
       if type(opts.ensure_installed) == "table" then
         ---@type table<string, boolean>
